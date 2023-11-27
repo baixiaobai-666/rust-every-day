7317847d6b8274369794411ef52c04fed16a4b57
‘baixiaobai <18375721476@sina.cn>
2023-11-28 00:45:47 +0800

    add for & while & loop demo

--- test-code/examples/string.rs
+++ test-code/examples/string.rs
@@ -52,12 +52,105 @@
 // // 老编译器中，r1、r2、r3作用域在这里结束
  // 新编译器中，r3作用域在这里结束
 
+// fn main() {
+//   let reference_to_nothing = dangle();
+// }
+
+// fn dangle() -> &String {
+//   let s = String::from("hello");
+
+//   &s
+// }
+
 fn main() {
-  let reference_to_nothing = dangle();
+  let condition = true;
+  if condition == true {
+    println!("condition is true");
+  } else {
+    println!("condition is false");
+  }
+  let condition1 = true;
+  let number = if condition1 {
+    5
+  } else {
+    6
+  };
+  println!("The number is {}", number);
+
+  let var1 = 10;
+  let var2 = if var1 >= 10 {
+    var1
+  } else {
+    var1 + 10
+  };
+  println!("{}", var2);
+  let mut v = 0;
+  for i in 1..10 {
+    v = if i == 9 {
+      continue;
+    } else {
+      i
+    }
+  }
+  println!("{}", var1); 
+
+  let o = Some(3);
+  let v = if let Some(x) = o {
+    x
+  } else {
+    0
+  };
+  println!("{}", v);
+  let res: Option<i32> = check();
+  println!("{:?}", Some(res)); 
+  for_some();
+  some_while();
+  some_loop();
 }
 
-fn dangle() -> &String {
-  let s = String::from("hello");
+fn check() -> Option<i32> {
+  let n: i32 = 6;
+  if  n % 4 == 0 {
+    Some(n - 4)
+  } else if  n % 3 ==0 {
+    Some(n - 3)
+  } else if n % 2 == 0 {
+    Some(n - 2)
+  } else {
+    Some(n)
+  }
+}
 
-  &s
+fn for_some() {
+  for i in 1..5 {
+    println!("{}", i);
+  }
+
+  let a = [1, 2, 3, 4, 5];
+  // iter 方法把数组变成一个迭代器
+  for (i, v) in a.iter().enumerate() {
+    println!("第{}个元素是{}", i + 1, v)
+  }
+}
+
+fn some_while() {
+  let mut n = 0;
+  while n <= 5 {
+    println!("{}", n);
+    n += 1;
+  }
+  println!("我执行完成了！！！")
+}
+
+fn some_loop() {
+  let mut n = 0;
+  loop {
+      if n > 5 {
+        break;
+      } else {
+        println!("{}", n);
+          n += 1;
+      }
+  }
+  println!("我执行完成了！！！")
 }
